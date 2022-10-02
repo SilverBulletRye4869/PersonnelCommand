a4f73e581b14a0dcc02580dfcd74d223eb9a5c2f
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2022-10-02 12:35:57 +0900

    出力方法の変更

--- punisherlist/main.js
+++ punisherlist/main.js
@@ -1,52 +1,53 @@
-let db;
-let arr = new Object();
+let db; //database
+let arr = new Object(); //データ格納用
+const type = ["id","uuid","type","reason","term","cond"] //表示項目
 
 function init(){
     firebase.initializeApp(firebaseConfig);
-    db = firebase.firestore();
-
-    let puni = db.collection("punisherlist");
-    puni.get().then(e =>{
-        e.forEach(d => {
-            arr[d.id] = d.data()
-            arr[d.id]["id"] = d.id;
-            if(d.id == arr["0"]["count"].toString())read(1,30);
-        })
+    db = firebase.firestore().collection("punisherlist");
+    db.doc("cnt").get().then((e)=>{
+        arr[e.id] = e.data();
+        read(1,30)
     })
-    
-    let row = document.createElement("tr")
-    let type =["ID","UUID","理由","期限","状態"]
-    type.forEach(e =>{
-        let th = document.createElement("th")
-        th.innerHTML= e;
-        row.appendChild(th)
-    })
-    document.getElementById("puni_th").appendChild(row)
-
-    
 }
 
 
 function read(start,end){
     
     let puni_td = document.getElementById("puni_td");
-    
-    end = Math.min(end, arr["0"]["count"])
-    console.log("test0")
-    const type = ["id","uuid","reason","term","cond"]
-    console.log(end)
+    end = Math.min(end, arr["cnt"]["cnt"]-1)
     for(let i = start; i<=end; i++){
-        
-        let row = document.createElement("tr");
-        type.forEach(e=>{
-            let td = document.createElement("td")
-            
-            td.innerHTML = arr[i.toString()][e];
-            row.appendChild(td)
-        })
-        puni_td.appendChild(row)
+        if(arr[i.toString()] == undefined){
+            db.doc(i.toString()).get().then((e)=>{
+                arr[i.toString()] = e.data()
+                arr[i.toString()]["id"] = e.id;
+                console.log(e.id)
+                puni_td.appendChild(dispOut(i,end))
+            })
+        }else puni_td.appendChild(dispOut(i,end))
     }
     
+    
+}
+function dispOut(i,end){
+    let row = document.createElement("tr");
+    type.forEach(e=>{
+        let td = document.createElement("td")     
+        td.innerHTML = arr[i.toString()][e];
+        row.appendChild(td)
+    })
+    if(i==end)load("fin");
+    return row
 }
 
+
+function load(type){
+    if(type=="fin"){
+        document.getElementById("loading").style.display="none"
+        document.getElementById("main").style.display="block"
+    }else{
+        document.getElementById("loading").style.display="block"
+        document.getElementById("main").style.display="none"
+    }
+}
 init();
